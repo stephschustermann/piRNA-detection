6c0eea0898969114f173a3d79d5c299f06a96e29
סטפני שוסטרמן <schustermann21@gmail.com>
2019-10-07 13:08:23 +0300

    https://github.com/users/stephschustermann/projects/1#card-26483199 https://github.com/users/stephschustermann/projects/1#card-26483214

--- setTE.py
+++ setTE.py
@@ -2,27 +2,34 @@
 import pandas as pd
 from collections import Counter
 
+df = []
+
 def getElandFileContent(path):
     content = []
     header = True
     with open(path)as f:
         for line in f:
             if header:
-                content.append(['probe_id', 'strand'])
+                content.append(['probe_id', 'strand', 'type'])
                 header = False
             else:
                 row = line.strip().split('\t')
+                print(row)
                 strand = row[-1]
+                teType = row[1]
+                if row[1].isdigit(): # a row with no type cell
+                    teType = ''
                 probe_id = ''
                 for cell in row:
                     if cell.startswith('HWI'):
                         probe_id = cell
-                content.append([probe_id, strand])
+                content.append([probe_id, strand, teType])
     return content
 
 
 # read TE instersection table with reads
 def readIntersectionWithSequences(intersectionFilePath):
+    intersectionFilePath = './featuresData/repbase.out.eland'
     content = getElandFileContent(intersectionFilePath)
     df = pd.DataFrame(data=content[1:], columns=content[0])
     column = list(df['probe_id'])
@@ -30,5 +37,6 @@ def readIntersectionWithSequences(intersectionFilePath):
 # if read exists in TE intersection table return true
 def mapsToTranposableElement(intersectionTEtable, sequenceId):
     if intersectionTEtable[sequenceId] > 0:
-        return True
+        return  df.loc[df['probe_id'] == sequenceId] # TODO https://github.com/users/stephschustermann/projects/1#card-27427098
     return False
+
